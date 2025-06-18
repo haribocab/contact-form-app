efe579095ad699ebd8ea3931b75b02b48bdf206a
Haruka Lange <haruka.lange@ds-extern.com>
2025-06-18 10:56:50 +0200

    updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPge from './pages/LoginPage';
 import ContactPage from './pages/ContactPage';
 import ProtectedRoute from './routes/ProtectedRoute';
+import HomePage from './pages/HomePage';
 
 export default function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPge />} />
         <Route 
           path="/contact" 
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:4000',
+export default defineConfig(({ mode }) => {
+  // Load environment variables
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': env.VITE_API_BASE,
+      },
     },
-  },
+  }
 })
