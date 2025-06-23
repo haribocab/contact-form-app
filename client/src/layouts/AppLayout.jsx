d46d248985f9327623a5abd5ff74370b27e3a6f8
Haruka Lange <haruka.lange@ds-extern.com>
2025-06-23 20:37:44 +0200

    updates

--- client/src/layouts/AppLayout.jsx
+++ client/src/layouts/AppLayout.jsx
@@ -1,7 +1,7 @@
 export default function AppLayout( {children} ) {
     return (
         <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-                {children}
+            {children}
         </div>
     )
 }
