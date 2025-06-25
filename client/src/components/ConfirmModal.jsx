86213ae811f33ad263a0543407e1f3a0e5b7c848
Haruka Lange <haruka.lange@ds-extern.com>
2025-06-25 14:41:34 +0200

    updates

--- client/src/components/ConfirmModal.jsx
+++ client/src/components/ConfirmModal.jsx
@@ -8,20 +8,32 @@ export default function ConfirmModal ({isOpen, onRequestClose, onConfirm, childr
             contentLabel="Confirm Delete"
             style={{
                 content: {
-                maxWidth: '400px',
-                margin: 'auto',
-                padding: '2rem',
-                borderRadius: '8px',
-                textAlign: 'center',
+                    maxWidth: '400px',
+                    margin: 'auto',
+                    padding: '2rem',
+                    borderRadius: '8px',
+                    textAlign: 'center',
+                    height: 'auto',
+                    maxHeight: '80vh',
+                    overflow: 'auto', 
+                    top: '50%',
+                    bottom: 'auto',
+                    transform: 'translateY(-50%)'
                 },
             }}
         >
         {children}
-        <div style={{ marginTop: '1.5rem' }}>
-            <button onClick={onRequestClose}>
+        <div className='flex justify-between mt-6'>
+            <button
+             onClick={onRequestClose}
+             className='bg-white px-4 py-2 border rounded'
+             >
                 Cancel
             </button>
-            <button onClick={onConfirm} className='bg-red-500'>
+            <button
+             onClick={onConfirm}
+             className='bg-primary px-4 py-2 border border-primary rounded'
+             >
                 Confirm
             </button>
         </div>
