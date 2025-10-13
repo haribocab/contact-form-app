1e6d19b717ca7cc5e263e32b2ba9b6cce175b3f7
Haruka Lange <haruka.lange@ds-extern.com>
2025-10-13 23:20:46 +0200

    uodates

--- client/src/layouts/AppLayout.jsx
+++ client/src/layouts/AppLayout.jsx
@@ -1,6 +1,14 @@
+import BackgroundImageUrl from '../images/background.jpg';
+
 export default function AppLayout( {children} ) {
     return (
-        <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+        <div 
+            className="min-h-screen bg-gray-100 flex items-center justify-center"
+            style={{ 
+                backgroundImage: `url(${BackgroundImageUrl})`,
+                backgroundSize: `cover`
+             }}
+         >
             {children}
         </div>
     )
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,12 +1,13 @@
 import AppLayout from '../layouts/AppLayout';
 import { Link } from 'react-router-dom';
 
+
 export default function HomePage() {
     return (
         <AppLayout>
             <div className="bg-gradient-to-br from-green-400 to-blue-500 flex flex-col items-center justify-center w-full max-w-md bg-white p-8 rounded-xl shadow-md border border-gray-200">
                 <h1 className="text-white text-5xl font-extrabold mb-8 drop-shadow-lg">
-                    Login Chat App
+                    Messages App
                 </h1>
                 <p className="text-white text-lg mb-12 max-w-lg text-center drop-shadow-md">
                     Please login to access your chat board and manage your data securely.
