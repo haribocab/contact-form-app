f6ba8ad5842c70bd1448c98b6ef2428e5124311d
Haruka Lange <haruka.lange@ds-extern.com>
2025-10-13 12:30:14 +0200

    update

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -6,6 +6,15 @@ use App\Http\Controllers\EntryController;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+Route::post('/test', function (\Illuminate\Http\Request $request) {
+    return response()->json([
+        'message' => 'Test endpoint accessed successfully!',
+        'method' => $request->method(),
+        'headers' => $request->headers->all(),
+        'body' => $request->all(),
+    ]);
+});
+
 Route::middleware('auth:api')->group(function () {
     Route::get('/entries', [EntryController::class, 'index']);
     Route::post('/entries', [EntryController::class, 'store']);
