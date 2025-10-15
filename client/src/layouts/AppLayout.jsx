bb97bb23837530009cdc7d337effbb8555189948
Haruka Lange <haruka.lange@ds-extern.com>
2025-10-15 17:07:36 +0200

    u

--- client/src/layouts/AppLayout.jsx
+++ client/src/layouts/AppLayout.jsx
@@ -1,15 +1,8 @@
-import BackgroundImageUrl from '../images/background.webp';
 import { ToastContainer } from 'react-toastify';
 
 export default function AppLayout( {children} ) {
     return (
-        <div 
-            className="min-h-screen bg-gray-100 flex items-center justify-center"
-            style={{ 
-                backgroundImage: `url(${BackgroundImageUrl})`,
-                backgroundSize: `cover`
-             }}
-         >
+        <div className="min-h-screen bg-gray-100 flex items-center justify-center">
             <ToastContainer />
             {children}
         </div>
